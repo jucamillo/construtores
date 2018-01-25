f1658ebb3382f546144241a000baa1bb23df0823
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-24 22:01:34 -0200

    ex. construtores 2

--- cartaoDeCredito.php
+++ cartaoDeCredito.php
@@ -9,9 +9,22 @@ class cartaoDeCredito
   public $numero; //atributo
   public $dataDeValidade; //atributo
   public $cliente; //atributo
+
+
+  function __construct($numero, $dataDeValidade, $cliente)
+    {
+      $this->numero = $numero;
+      $this->dataDeValidade = $dataDeValidade;
+      $this->cliente = $cliente;
+      echo "Cartao Criado: $numero" . PHP_EOL;
+      echo "Data Validade: $dataDeValidade" . PHP_EOL;
+      echo "Cliente: $cliente" . PHP_EOL;
+    }
 }
 
 
-$cartao = new cartaoDeCredito();
-$cartao->numero = 12910290192;
-$cartao->dataDeValidade = Carbon::createFromDate(2023, 5, 21, 'America/Sao_Paulo');
+$cartao = new cartaoDeCredito(1212121, Carbon::createFromDate(2023, 5, 21, 'America/Sao_Paulo'), 'Lorem');
+
+
+
+$cartao = new cartaoDeCredito(23233333, Carbon::createFromDate(2018, 5, 21, 'America/Sao_Paulo'), 'Ipsum');
